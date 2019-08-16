69cf7f61ef65d40b8087bfb9e211a3058c6efa50
Tu Nam Le <tle21@ssis.edu.vn>
2019-08-16 08:23:20 +0700

    Tu Nam's Fahrenheit Project

---
+++ fahrenheit.py
@@ -0,0 +1,11 @@
+#Program prompts user for the Celcius input
+#Program converts Celcius input into Fahrenheit
+#Program prints the Calculated Fahrenheit output
+
+
+print ("Tu Nam's Celcius to Fahrenheit Converter")
+Celcius = input ("Celcius : ") 
+Fahrenheit =float(Celcius) * 1.8 + 32
+print ("Fahrenheit :", Fahrenheit, "F")
+
+
